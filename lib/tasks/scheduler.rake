97be9eb666af88909e14fd6425253714f243c40e
Ryan Kulp <ryanckulp@gmail.com>
2016-11-18 09:25:33 -0600

    rearrange rake tasks

---
+++ lib/tasks/scheduler.rake
@@ -0,0 +1,4 @@
+desc "Imports tweets matching keywords in database"
+task import_tweets: :environment do
+  TwitterService.import_tweets
+end
--- lib/tasks/twitter/import_tweets.rb
+++
@@ -1,6 +0,0 @@
-namespace :twitter do
-  desc "Imports tweets matching keywords in database"
-  task import_tweets: :environment do
-    TwitterService.import_tweets
-  end
-end
